18116f6272a0d8d52888f4de2339f7d80ecbf72b
Gabriele Romano <gaggioaxel@gmail.com>
2023-11-02 18:57:47 +0100

    cleanup and thesis update

---
+++ code/src/clustering.py
@@ -0,0 +1,344 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+
+################### remapping dataframes
+def group_table_by_joints(table: pd.DataFrame) -> pd.DataFrame:
+    columns = [col.replace('_X','') for col in list(table.columns)[::3]]
+    result = pd.DataFrame(columns=columns)
+    for j in range(0,table.shape[1],3):
+        lst = []
+        for i in range(table.shape[0]):
+            lst.append(table.iloc[i,j:j+3].values)
+        result[columns[int(j/3)]] = np.array(lst)
+    return result
+
+# Not used
+def group_table_by_jointsV2(table: pd.DataFrame) -> pd.DataFrame:
+    # Get unique joint names by removing '_X' from column names
+    columns = [col.replace('_X', '') for col in table.columns[::3]]
+
+    # Group the DataFrame by joint name and aggregate the grouped values as lists
+    result = table.groupby([col.replace('_X', '') for col in table.columns], axis=1).agg(lambda x: x.values.tolist())
+
+    # Rename the columns to match the joint names
+    result.columns = columns
+
+    return result
+
+
+def joints_array_to_xyz_columns(table: pd.DataFrame) -> pd.DataFrame:
+    return pd.concat([table[column].apply(lambda row: pd.Series(row,index=[column+'_X',column+'_Y',column+'_Z'])) for column in table.columns],axis=1)
+    
+def table_to_list_xyz_tables(table:pd.DataFrame,into="xyz"):
+    if into == "xyz":
+        return table.iloc[:,::3],table.iloc[:,1::3],table.iloc[:,2::3]
+    elif into == "points":
+        return [table.iloc[:,j:j+3] for j in range(0,table.shape[1],3)]
+
+def xyz_tables_to_xyz_columns(tablesList):
+    xTable,yTable,zTable = tablesList
+    mergedTable = pd.DataFrame()
+    for joint in range(xTable.shape[1]):
+        mergedTable = pd.concat([mergedTable,xTable.iloc[:,joint],yTable.iloc[:,joint],zTable.iloc[:,joint]],axis=1)
+    return mergedTable
+####################
+
+
+#################### reproducing MATLAB movmean and movmedian
+def movmean(x, w):
+    return pd.Series(x).rolling(window=w,min_periods=1,center=True).mean()
+
+from scipy.signal import medfilt
+def movmedian(x,w):
+    return np.concatenate([np.convolve(x[:w-1], np.ones(w-1), 'valid') / (w-1),medfilt(x,w)[1:-1],np.convolve(x[-w+1:], np.ones(w-1), 'valid') / (w-1)])
+###################
+
+################### features computation
+def smoothing(x: pd.DataFrame) -> pd.DataFrame:
+    return x.apply(lambda col: movmedian(col,5)).apply(lambda col: movmean(col,25))
+    
+def compute_derivatives(x:pd.DataFrame,dt:float,smooth=True) -> pd.DataFrame:
+    v : pd.DataFrame
+    v = x.diff() / dt
+    v = v.fillna(v.iloc[1:10,:].mean())
+    return smoothing(v) if smooth else v
+###################
+
+def calculate_weight_matrix(featuresTable: pd.DataFrame, adjacencyMatrix: np.ndarray,tol:float=None,use_cosine=True) -> np.ndarray:
+    """
+    Calculate the weight matrix for a given set of features and adjacency matrix.
+
+    Parameters:
+        - featuresTable (pd.DataFrame): A DataFrame containing the features as rows.
+        - adjacencyMatrix (np.ndarray): An adjacency matrix indicating connections between data points.
+        - tol (float, optional): A tolerance value used for scalar features. Default is None.
+        - use_cosine (bool, optional): Use cosine similarity for non-scalar features. Default is True.
+
+    Returns:
+        - weight_matrix (np.ndarray): A matrix of weights reflecting the pairwise relationships between data points.
+
+    Notes:
+        - If features are non-scalar (e.g., vectors), cosine similarity is used by default.
+        - If features are scalar (e.g., single values), you can specify a tolerance value (tol).
+        - The weight matrix is symmetric and represents the strength of connections between data points.
+
+    Example:
+        # Calculate the weight matrix for non-scalar features with cosine similarity
+        weight_matrix = calculate_weight_matrix(featuresTable, adjacencyMatrix)
+
+        # Calculate the weight matrix for scalar features with a tolerance value
+        weight_matrix = calculate_weight_matrix(featuresTable, adjacencyMatrix, tol=0.01, use_cosine=False)
+    """
+    if isinstance(featuresTable.iloc[0],np.ndarray):
+        values = np.stack(featuresTable.values)
+        if not use_cosine:
+            norm_matrix = np.linalg.norm(values, axis=1)
+            norm_crossed_matrix = norm_matrix[:, None] * norm_matrix
+            dot_crossed_matrix = np.dot(values, values.T)
+            #                           where                       True                                                False
+            weight_matrix = np.where(adjacencyMatrix, dot_crossed_matrix / norm_crossed_matrix + 1, dot_crossed_matrix / (norm_crossed_matrix + 1) * 1/5)
+        else:
+            weight_matrix = np.where(adjacencyMatrix, cosine_similarity(values)+1, 0)
+    else:
+        if tol is None:
+            raise Exception('must provide tol value with scalar feature')
+        diff_matrix = featuresTable.values[:, None] - featuresTable.values
+        # norm matrix not needed since features in our case are already scalar
+        norm_matrix = np.abs(diff_matrix) #np.linalg.norm(diff_matrix, axis=-1)
+        if not use_cosine:
+            #                           where                   True                    False
+            weight_matrix = np.where(adjacencyMatrix, 1 / (norm_matrix + tol)+5, 1 / (5 * norm_matrix + tol+2))
+        else:
+            weight_matrix = np.where(adjacencyMatrix, cosine_similarity(norm_matrix)+1,0)
+    np.fill_diagonal(weight_matrix,0)
+    #if normalize:
+    #    weight_matrix = 2*(weight_matrix - np.min(weight_matrix,axis=None)) / (np.max(weight_matrix,axis=None) - np.min(weight_matrix,axis=None))
+    return weight_matrix
+
+# Spectral clustering using Shi and Malik algorithm
+def shi_malik_spectral_clustering(weightMatrix:np.ndarray) -> np.array:
+    laplacian_matrix = np.diag(np.array(weightMatrix.sum(axis=0))) - weightMatrix
+    eigenvalues, eigenvectors = np.linalg.eig(laplacian_matrix)
+
+    idx = np.argsort(-eigenvalues)[1]
+    new_features = eigenvectors[:,idx]
+    return (new_features > 0).astype(int)
+
+def myeigs(S: np.ndarray,unused_k=2):
+    #S = pd.read_csv('S.csv',header=None).to_numpy()
+    Sdiff = S-S.T
+    eigenvalues, eigenvectors = np.linalg.eig(S)
+    if np.max(Sdiff) < 10**-20: # symmetric matrix, don't know how to implement largest absolute option
+        raise ValueError("Symmetric matrix S, not implemented")
+    idx = np.argsort(-eigenvalues)[1]
+    return eigenvectors[:,idx].T, eigenvalues[idx]
+
+def split_a_cluster(S, ww, nmin, optimizationOptions=None): # optimizationOptions is always ncut in this instance
+    n = len(ww)
+    tt = np.sum(S,axis=0)
+    iPerm = np.argsort(ww)
+    #ww = ww[iperm] ww not used anymore in this instance
+
+    nCuts = np.zeros(n-1)
+    for iCut in range(n-1):
+        i1 = np.arange(iCut+1)
+        i2 = np.arange(iCut+1,n)
+        i1 = iPerm[i1]
+        i2 = iPerm[i2]
+        mult_factor = 1/np.sum(tt[i1]) + 1/np.sum(tt[i2])
+        nCuts[iCut] = np.sum(S[i1,:][:,i2]) * mult_factor
+    iMin = np.argmin(nCuts)
+    i1 = np.arange(iMin+1)
+    i2 = np.arange(iMin+1,n)
+    elems1 = iPerm[i1]
+    elems2 = iPerm[i2]
+
+    if len(i1) > nmin:
+        S1 = S[elems1,:][:,elems1]
+        T1 = np.diag(np.sum(S1,axis=0))
+        P1 = np.linalg.lstsq(T1,S1,rcond=None)[0]
+        ww1, lambda1 =  myeigs(P1,2)
+    else:
+        lambda1 = -2
+        ww1 = np.ones(len(i1))
+
+    if len(i2) > nmin:
+        S2 = S[elems2,:][:,elems2]
+        T2 = np.diag(np.sum(S2,axis=0))
+        P2 = np.linalg.lstsq(T2,S2,rcond=None)[0]
+        ww2,lambda2 = myeigs(P2,2)
+    else:
+        lambda2 = -2
+        ww2 = np.ones((len(i2)))
+    return elems1,elems2,lambda1,lambda2,ww1,ww2
+
+
+def shi_malik_spectral_clustering_matlab_version(S: np.ndarray, k:int=4):
+    n = len(S)
+    T = np.sum(S,axis=1)
+    P = S / np.tile(T[:,np.newaxis],len(T))
+    nmin = 5
+    KMAX = min(10,2*k)
+
+    shi_nclust = 0
+    shi_ww = np.zeros((KMAX,n))
+    shi_elems = np.zeros((KMAX,n),dtype=int)
+    shi_n = np.zeros(KMAX,dtype=int)
+    shi_lambda = np.zeros(KMAX)
+    assignment_shi_r = np.zeros(n,dtype=int)
+
+    shi_elems[0,:] = np.arange(n)
+    shi_n[0] = n
+    
+    isplit = 0
+    shi_ww[0,:] = myeigs(P,2)[0]
+    while shi_nclust < k-1: 
+        elems = shi_elems[isplit,:shi_n[isplit]].copy()
+        elems1,elems2,lambda1,lambda2,ww1,ww2 = split_a_cluster(S[elems,:][:,elems],shi_ww[isplit,:shi_n[isplit]],nmin,None)
+        nn1 = len(elems1)
+        nn2 = len(elems2)
+        shi_nclust+=1
+        shi_elems[isplit,:nn1] = elems[elems1]
+        shi_elems[shi_nclust,:nn2] = elems[elems2] 
+        shi_lambda[isplit] = lambda1
+        shi_lambda[shi_nclust] = lambda2
+        shi_ww[isplit,:nn1] = ww1
+        shi_ww[shi_nclust,:nn2] = ww2
+        shi_n[isplit] = nn1
+        shi_n[shi_nclust] = nn2
+        ldummy, isplit = np.max(shi_lambda[:shi_nclust+1]), np.argmax(shi_lambda[:shi_nclust+1])
+        if ldummy == -2:
+            print('cant find k clusters with these conditions')
+    for iclust in range(k):
+        assignment_shi_r[shi_elems[iclust,:shi_n[iclust]]] = iclust
+    return assignment_shi_r
+
+
+from pandas import Series
+from numpy import array, sum
+from itertools import permutations
+from typing import Literal
+from networkx.algorithms.bipartite import minimum_weight_full_matching as min_wei_ful_match
+import networkx as nx
+from matplotlib import pyplot as plt
+
+def compute_cost(fromLabels:list,toLabels:list,labels_kind:Literal['full','cluster']='full'):
+    if labels_kind == 'full':
+        assert len(fromLabels) == len(toLabels)
+        return sum(array(fromLabels) != array(toLabels))
+    elif labels_kind =='cluster':
+        return len(set(toLabels).difference(set(fromLabels)))
+    raise Exception("method not implemented")
+
+def clusterize_labels(labels:list): # assuming labels as list of integers going from 0 to n
+    df = Series(labels)
+    clusters = df.groupby(df).groups
+    keys = set(clusters.keys())
+    for i in range(max(keys)):
+        if i not in keys:
+            clusters[i] = []
+    return clusters
+
+def labelize_clusters(clusters:dict):
+    clusterLabels = np.zeros(sum(len(nodes) for nodes in clusters.values()), dtype=int)
+    for clusterLabel, indices in clusters.items():
+        clusterLabels[indices] = clusterLabel
+    return clusterLabels
+
+def compute_minimum_weight_cluster(fromLabels:list,toLabels:list,method:Literal['BF','MWPM']="BF",visualize=False,return_relabeling=False) -> tuple :
+    # Reassign labels 
+    out_elements = []
+    if method.upper() == "BF":
+        toClusters = clusterize_labels(toLabels)
+        clusterLabelPermutations = list(permutations(list(toClusters.keys())))
+        toClustersValues = list(toClusters.values())
+        optimalLabelingIndex = -1
+        minCost = 10**5
+        if visualize:
+            leftNodes = [0]
+            rightNodes = list(range(1,len(clusterLabelPermutations)+1))
+            plt.figure(figsize=(8,8))
+            G = nx.DiGraph()
+            G.add_nodes_from(leftNodes,bipartite=0,color='#1f78b4')
+            G.add_nodes_from(rightNodes,bipartite=1,color='#33a02c')
+            pos = nx.bipartite_layout(G,leftNodes)
+            x_center = pos[leftNodes[0]][0]
+            y_center = pos[leftNodes[0]][1]
+            for node in rightNodes:
+                angle = (2 * node * 3.14159) / len(rightNodes)  # Distribute the right nodes evenly around the center node
+                distance = 2.0  # Adjust this value to control the distance of the right nodes from the center node
+                x = x_center + distance * np.cos(angle)
+                y = y_center + distance * np.sin(angle)
+                pos[node] = (x, y)
+            
+        for indexPermutation,clusterLabelPermutation in enumerate(clusterLabelPermutations):
+            permCost = compute_cost(fromLabels,labelize_clusters(dict(zip(clusterLabelPermutation,toClustersValues))))
+            if visualize:
+                G.add_edge(leftNodes[0],rightNodes[indexPermutation],weight=permCost,color='b',width=1)
+            if permCost < minCost:
+                optimalLabelingIndex = indexPermutation
+                minCost = permCost
+        if visualize:
+            edgeColors = ['b' if i == optimalLabelingIndex else '#C4C2C6' for i in range(len(rightNodes))]
+            edgeWidths = [3 if i == optimalLabelingIndex else 1 for i in range(len(rightNodes))]
+            nx.draw(G, pos=pos,with_labels=True,node_size=900,node_color=list(nx.get_node_attributes(G, 'color').values()),edge_color=edgeColors,width=edgeWidths)
+            nx.draw_networkx_edge_labels(G, pos, edge_labels=nx.get_edge_attributes(G,'weight'), label_pos=0.5)
+        out_elements.append(labelize_clusters(dict(zip(clusterLabelPermutations[optimalLabelingIndex],toClustersValues))))
+        if return_relabeling:
+            out_elements.append(clusterLabelPermutations[optimalLabelingIndex])
+    elif method.upper() == 'MWPM':
+        fromClustersMap = clusterize_labels(fromLabels)
+        clusters = list(fromClustersMap.keys())
+        toClustersUnlabelized = list(clusterize_labels(toLabels).values())
+        rightNodes = (np.array(clusters) + max(clusters)+1).tolist()
+        color_pool = ['#e31a1c', '#1f78b4', '#33a02c', '#ff7f00', '#6a3d9a', '#b15928']
+
+        edgeWeights = {fromLabel : 
+                            {toLabelAssigned+max(clusters)+1 : 
+                                {'weight': compute_cost(fromClustersMap[fromLabel],toClustersUnlabelized[toLabelAssigned],labels_kind='cluster')}
+                             for toLabelAssigned in clusters } 
+                         for fromLabel in clusters }
+        
+        G = nx.from_dict_of_dicts(edgeWeights,create_using=nx.DiGraph)
+        
+        minWeightsEdges = {fromNode:toNode for fromNode, toNode in min_wei_ful_match(G,clusters,'weight').items() if fromNode not in rightNodes}
+        reversedWeightsEdgesDict = {value:key for key,value in minWeightsEdges.items()}
+        if visualize:
+            plt.figure(figsize=(11,8))
+            pos = nx.bipartite_layout(G, clusters)
+            nx.draw(G, 
+                    pos,
+                    with_labels=True,
+                    labels={elemFrom:elemFrom%len(clusters) for elemFrom in clusters+rightNodes}, 
+                    node_size=1000, 
+                    node_color= [color_pool[nodeId] for nodeId in clusters] +
+                                [color_pool[reversedWeightsEdgesDict[toNodeId]] for toNodeId in sorted(reversedWeightsEdgesDict.keys())], 
+                    font_size=10,
+                    edge_color=[color_pool[edge[0]] if edge in list(minWeightsEdges.items()) else '#C4C2C6' for edge in G.edges()],
+                    width=[3 if edge in list(minWeightsEdges.items()) else 1 for edge in G.edges()])
+            nx.draw_networkx_edge_labels(G, 
+                                         pos, 
+                                         edge_labels=nx.get_edge_attributes(G, 'weight'),
+                                         label_pos=0.85,
+                                         font_size=10)#,
+                                         #font_weight='bold')
+            for node in G.nodes:
+                text = list(fromClustersMap[node]) if node <= max(clusters) else list(toClustersUnlabelized[node-max(clusters)-1])
+                text = list(map(str,text))
+                text = '[ '+',\n'.join(', '.join(text[i:i+3]) for i in range(0, len(text), 3))+' ]'
+                
+                plt.text(pos[node][0], 
+                         pos[node][1]+0.11, 
+                         text,#str(list(fromClustersMap[node]))[1:-1] if node <= max(clusters) else str(list(toClustersUnlabelized[node-max(clusters)-1]))[1:-1], 
+                         ha='center', 
+                         va='center', 
+                         color='black', 
+                         fontsize=10,
+                         fontweight='bold')
+            #plt.suptitle('Minimum weight clusters',fontsize=14,fontweight='bold')
+            plt.show()
+        out_elements.append(labelize_clusters({fromNode: toClustersUnlabelized[minWeightsEdges[fromNode]-max(clusters)-1] for fromNode in minWeightsEdges.keys()}))
+        if return_relabeling:
+            out_elements.append({fromNode: minWeightsEdges[fromNode]-max(clusters)-1 for fromNode in minWeightsEdges.keys()})
+    return out_elements if len(out_elements) > 1 else out_elements[0]
